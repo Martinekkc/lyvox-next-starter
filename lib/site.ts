a8260f732bbb9174464e11327e2b64142fb3563b
Martin Cerny <109151994+Martinekkc@users.noreply.github.com>
2026-05-19 18:15:50 +0200

    Add site configuration

---
+++ lib/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "LYVOX Next Starter",
+  url: "https://lyvoxdigital.com",
+  description:
+    "A modern Next.js starter for agencies, freelancers and small businesses.",
+  author: "Martin Cerny",
+  links: {
+    github: "https://github.com/Martinekkc/lyvox-next-starter",
+    website: "https://lyvoxdigital.com"
+  }
+};
